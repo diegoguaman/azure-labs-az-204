bd5b40f71d4bff6d6dea06b948fc5020a5b2cea3
Diego Guaman Medina <diegoguamanmedina@gmail.com>
2026-02-26 09:38:52 +0000

    Agregar implementación inicial de la aplicación con Azure Blob Storage y archivo de prueba

---
+++ prueba-practica/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+
+class Program
+{
+    static async Task Main()
+    {
+        string connectionString = Environment.GetEnvironmentVariable("AZURE_STORAGE_CONNECTION_STRING");
+        if (string.IsNullOrEmpty(connectionString))
+        {
+            Console.WriteLine("Hace falta la variable AZURE_STORAGE_CONNECTION_STRING");
+            return;
+        }
+
+        string containerName = "archivos";
+        string blobName = "mensaje.txt";
+
+        var blobClient = new BlobClient(connectionString, containerName, blobName);
+        var download = await blobClient.DownloadAsync();
+
+        using var reader = new StreamReader(download.Value.Content);
+        string contenido = await reader.ReadToEndAsync();
+        Console.WriteLine("Contenido del blob:");
+        Console.WriteLine(contenido);
+    }
+}
